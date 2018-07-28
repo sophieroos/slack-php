b6e8e7dfc5b6b51d18e3bc2cf1175682274916c6
unknown <sophie@famroos.nu>
2018-07-28 21:47:36 +0200

    Custom emojis ophalen van Slack en toevoegen aan cache bestand

--- app.php
+++ app.php
@@ -6,11 +6,13 @@ define('DIR_BASE', __DIR__ . '/');
 define('DIR_APP', DIR_BASE . 'App/');
 define('DIR_TASK', DIR_BASE . 'Task/');
 define('DIR_MODEL', DIR_BASE . 'Model/');
+define('DIR_CACHE', DIR_BASE . 'Cache/');
 
 include DIR_BASE . 'GuzzleClient.php';
 include DIR_BASE . '.env.php';
 include DIR_BASE . 'emojis.php';
 include DIR_BASE . 'setup.php';
+include DIR_BASE . 'cache.php';
 
 include DIR_TASK . 'Task.php';
 include DIR_TASK . 'SendMessage.php';
