f61fa4a6cc033ae064db74db2c8a8196e24847ed
unknown <sophie@famroos.nu>
2018-08-17 09:28:42 +0200

    Moved functions about entering channel to seperate file

--- slack.php
+++ slack.php
@@ -36,33 +36,6 @@ while (true) {
  * @param $line
  * @param $client
  */
-function enter_channel($line, $client)
-{
-    $channelId = array_search($line, $_ENV['channels-lower-case'], false);
-    $parameters = [$channelId];
-    $last_messages = new GetLastMessages($parameters);
-
-    $last_messages = $last_messages->execute($client);
-
-    foreach ($last_messages as $message) {
-        echo $message, PHP_EOL;
-    }
-    echo $_ENV['current_user']->createStyledString();
-
-    $_SESSION['entered_channel'] = $channelId;
-}
-
-
-function exit_channel()
-{
-    $_SESSION['entered_channel'] = false;
-    echo 'Exited' . PHP_EOL;
-}
-
-/**
- * @param $line
- * @param $client
- */
 function run_task($line, $client)
 {
     $line = str_replace(':', ' ', $line);
@@ -85,19 +58,4 @@ function run_task($line, $client)
         }
         echo $results;
     }
-}
-
-/**
- * @param $message
- * @param $channelId
- * @param $client
- */
-function send_message($message, $channelId, $client)
-{
-    $sendMessage = new SendMessage();
-    $sendMessage->withChannel($channelId)->withMessage(new Message($message));
-
-    $sendMessage->execute($client);
-
-    echo $_ENV['current_user']->createStyledString();
 }
